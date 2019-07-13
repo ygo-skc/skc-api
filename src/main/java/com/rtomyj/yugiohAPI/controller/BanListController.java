b1b5d94db6b038a8c8118bb91f235e91b97c81a9
Javi Gomez <rtomyj@gmail.com>
2019-07-13 18:38:44 -0500

    Changed structure
    Modified code to work with new Card.java method. Added new endpoint to get ban list dates

--- src/main/java/com/rtomyj/yugiohAPI/controller/BanListController.java
+++ src/main/java/com/rtomyj/yugiohAPI/controller/BanListController.java
@@ -3,65 +3,49 @@ package com.rtomyj.yugiohAPI.controller;
 import com.rtomyj.yugiohAPI.Card;
 import com.rtomyj.yugiohAPI.repository.BanListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+
+import static com.rtomyj.yugiohAPI.Card.toJSON;
 
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
-@RequestMapping(path="/ban_list")
 public class BanListController {
     @Autowired
     BanListRepository banListRepository;
 
-    @GetMapping
-    public String check()
+    @RequestMapping(path="/ban_list", method = RequestMethod.GET, produces = "application/json; charset=utf-8" )
+    @ResponseBody
+    public String allCurrentBannedCards()
     {
         String cards = "{ \"bannedCards\": {\n";
-        ArrayList<Card> bannedCards = (ArrayList) banListRepository.getForbiddenCards();
 
         /*  Forbidden cards */
         cards += "\"forbidden\": [\n";
-
-        for (int ind = 0; ind < bannedCards.size(); ind++)
-        {
-            cards += bannedCards.get(ind).toJSON();
-
-            if (ind + 1 != bannedCards.size())  cards += ", \n";
-        }
+        cards += toJSON( (ArrayList<Card>) banListRepository.getForbiddenCards() );
         cards += "\n ] ";
 
         /*  Limited cards */
-        cards += ", \"limited\": [\n";
-
-        bannedCards = (ArrayList) banListRepository.getLimitedCards();
-        for (int ind = 0; ind < bannedCards.size(); ind++)
-        {
-            cards += bannedCards.get(ind).toJSON();
-
-            if (ind + 1 != bannedCards.size())  cards += ", \n";
-        }
+        cards += ", \"limited\": [\n";;
+        cards += toJSON( (ArrayList<Card>) banListRepository.getLimitedCards() );
         cards += "\n ] ";
 
         /*  Semi-Limited cards */
         cards += ", \"semiLimited\": [\n";
-
-        bannedCards = (ArrayList) banListRepository.getSemiLimitedCards();
-        for (int ind = 0; ind < bannedCards.size(); ind++)
-        {
-            cards += bannedCards.get(ind).toJSON();
-
-            if (ind + 1 != bannedCards.size())  cards += ", \n";
-        }
+        cards += toJSON( (ArrayList<Card>) banListRepository.getSemiLimitedCards() );
         cards += "\n ] ";
 
         cards += "\n } }";
         System.out.println(cards);
         return cards;
+    }
+
+    @RequestMapping(path="ban_list/all")
+    public String test()
+    {
+        return banListRepository.getBanListDates().toString();
     }
 
 }
