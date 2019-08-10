3e043d50e5b87aed6d26af4600aaf6f4127babbd
Javi Gomez <rtomyj@gmail.com>
2019-08-10 10:15:58 -0500

    Added RegEx validation
    Incomming requested ban list dates are checked with a regex

--- src/main/java/com/rtomyj/yugiohAPI/controller/BanListController.java
+++ src/main/java/com/rtomyj/yugiohAPI/controller/BanListController.java
@@ -20,6 +20,13 @@ public class BanListController {
 	@ResponseBody
 	public String allCurrentBannedCards()
 	{
+		Pattern datePattern = Pattern.compile("[0-9]{4}-[0-9]{2}-[0-9]{2}");
+		if (! datePattern.matcher(startDate).matches())
+		{
+			return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
+		}
+
+
 		String cards = "{ \"bannedCards\": {\n";
 
 		/*  Forbidden cards */
