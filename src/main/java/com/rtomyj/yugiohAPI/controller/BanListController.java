f975a65eb6fd306645a6267b6417582b107ae41f
Javi Gomez <rtomyj@gmail.com>
2019-08-10 10:16:35 -0500

    Added path variable for date

--- src/main/java/com/rtomyj/yugiohAPI/controller/BanListController.java
+++ src/main/java/com/rtomyj/yugiohAPI/controller/BanListController.java
@@ -16,9 +16,9 @@ public class BanListController {
 	@Autowired
 	BanListRepository banListRepository;
 
-	@RequestMapping(path="/ban_list", method = RequestMethod.GET, produces = "application/json; charset=utf-8" )
+	@RequestMapping(path="/ban_list/{startDate}", method = RequestMethod.GET, produces = "application/json; charset=utf-8" )
 	@ResponseBody
-	public String allCurrentBannedCards()
+	public ResponseEntity<String> allCurrentBannedCards(@PathVariable String startDate)
 	{
 		Pattern datePattern = Pattern.compile("[0-9]{4}-[0-9]{2}-[0-9]{2}");
 		if (! datePattern.matcher(startDate).matches())
