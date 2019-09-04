4b5bd078c1dd21ce730017a5405e2063e056457e
Javi Gomez <rtomyj@gmail.com>
2019-09-04 15:36:55 -0500

    Changed behavior. End point now needs a card ID not the card name to fetch.

--- src/main/java/com/rtomyj/yugiohAPI/controller/CardController.java
+++ src/main/java/com/rtomyj/yugiohAPI/controller/CardController.java
@@ -8,6 +8,7 @@ import com.rtomyj.yugiohAPI.repository.CardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,15 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping(path = "card/v1/")
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class CardController
 {
 	@Autowired
 	CardRepository cardRepository;
 
 
-	@GetMapping("/{cardName}")
-	public ResponseEntity<LinkedHashMap<String, String>> getCard(@PathVariable("cardName") String cardName)
+	@GetMapping("/{cardID}")
+	public ResponseEntity<LinkedHashMap<String, String>> getCard(@PathVariable("cardID") String cardID)
 	{
-		return new ResponseEntity<>(Card.toHashMap(cardRepository.getCardInfo(cardName)), HttpStatus.OK);
+		return new ResponseEntity<>(Card.toHashMap(cardRepository.getCardInfo(cardID)), HttpStatus.OK);
 	}
 }
--- src/main/java/com/rtomyj/yugiohAPI/repository/CardRepository.java
+++ src/main/java/com/rtomyj/yugiohAPI/repository/CardRepository.java
@@ -17,17 +17,17 @@ public class CardRepository
 	@Autowired
 	JdbcTemplate jdbcConn;
 
-	public Card getCardInfo(String cardName)
+	public Card getCardInfo(String cardID)
 	{
 		return jdbcConn.query(String.format(
-			"SELECT monster_type, color_id, card_effect, card_number, card_attribute, monster_attack, monster_defense FROM cards WHERE card_name = '%s'",
-			cardName), new ResultSetExtractor<Card>()
+			"SELECT card_name, monster_type, color_id, card_effect, card_attribute, monster_attack, monster_defense FROM cards WHERE card_number = '%s'",
+				cardID), new ResultSetExtractor<Card>()
 			{
 				@Override
 				public Card extractData(ResultSet row) throws SQLException, DataAccessException
 				{
 					row.next();
-					Card card = new Card(cardName, row.getString(1), "card_color", row.getString(3), row.getString(4),
+					Card card = new Card(row.getString(1), row.getString(2), "card_color", row.getString(4), cardID,
 							row.getString(5), row.getInt(6), row.getInt(7));
 					return card;
 				}
