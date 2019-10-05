9f053ae940fd3d8bf63307fba5c727903f97ab03
Javi Gomez <rtomyj@gmail.com>
2019-10-05 13:44:55 -0500

    log4j2 installation and implementation

--- src/main/java/com/rtomyj/yugiohAPI/controller/BannedCardsController.java
+++ src/main/java/com/rtomyj/yugiohAPI/controller/BannedCardsController.java
@@ -3,6 +3,8 @@ package com.rtomyj.yugiohAPI.controller;
 import com.rtomyj.yugiohAPI.model.Card;
 import com.rtomyj.yugiohAPI.service.BannedCardsService;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,15 +23,18 @@ public class BannedCardsController {
 	@Autowired
 	BannedCardsService bannedCardsService;
 
+	private static final Logger LOG = LogManager.getLogger(BannedCardsController.class.getName());
+
 	@ResponseBody
 	@GetMapping(path = "{startDate}")
-	public ResponseEntity<HashMap<String, LinkedHashMap<String, List<HashMap<String, String>>>>> getBannedCards(
-			@PathVariable String startDate)
+	public ResponseEntity<HashMap<String, LinkedHashMap<String, List<HashMap<String, String>>>>> getBannedCards(@PathVariable String startDate)
 	{
 		Pattern datePattern = Pattern.compile("[0-9]{4}-[0-9]{2}-[0-9]{2}");
 		if (! datePattern.matcher(startDate).matches())
 		{
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			HttpStatus status = HttpStatus.NOT_FOUND;
+			LOG.info(String.format("List requested: { %s } - responding with: { %s }", startDate, status));
+			return new ResponseEntity<>(status);
 		}
 
 		HashMap<String, LinkedHashMap<String, List<HashMap<String, String>>>> banList = new HashMap<>();
@@ -39,9 +44,18 @@ public class BannedCardsController {
 		banListSections.put("limited", Card.toHashMap(bannedCardsService.getBanListByBanStatus(startDate, "Limited")));
 		banListSections.put("semiLimited", Card.toHashMap(bannedCardsService.getBanListByBanStatus(startDate, "Semi-Limited")));
 
-		banList.put("bannedCards", banListSections);
-
-		return new ResponseEntity<>(banList, HttpStatus.OK);
-
+		if (banListSections.get("forbidden").size() == 0 && banListSections.get("limited").size() == 0 && banListSections.get("semiLimited").size() == 0)
+		{
+			HttpStatus status = HttpStatus.NO_CONTENT;
+			LOG.info(String.format("List requested: { %s } - responding with: { %s }", startDate, status));
+			return new ResponseEntity<>(status);
+		}
+		else
+		{
+			HttpStatus status = HttpStatus.OK;
+			LOG.info(String.format("List requested: { %s } - responding with: { %s }", startDate, status));
+			banList.put("bannedCards", banListSections);
+			return new ResponseEntity<>(banList, status);
+		}
 	}
 }
