0002373e707bba18a8373e1a0f8a3e080b9c9694
Javi Gomez <rtomyj@gmail.com>
2019-11-09 17:38:11 -0600

    Using Lombok to auto gen setter/getters and builders

--- src/main/java/com/rtomyj/yugiohAPI/model/Card.java
+++ src/main/java/com/rtomyj/yugiohAPI/model/Card.java
@@ -6,21 +6,25 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import lombok.Builder;
+import lombok.Data;
+
 /**
  * Class defines properties a Yugioh card can contain
  */
+@Data
+@Builder
 public class Card
 {
 	/** Name of the card */
-	private String cardName, monsterType, cardColor, cardEffect, cardID, cardAttribute;
-	private int monsterAttack, monsterDefense;
-
-
-
-	/**
-	 * Default constructor
-	 */
-	public Card() {}
+	private String cardName;
+	private String monsterType;
+	private String cardColor;
+	private String cardEffect;
+	private String cardID;
+	private String cardAttribute;
+	private int monsterAttack;
+	private int monsterDefense;
 
 
 
@@ -67,121 +71,5 @@ public class Card
 		cardInfo.put("monsterDef", Integer.toString(card.monsterDefense));
 
 		return cardInfo;
-	}
-
-
-
-	public String getCardName() { return this.cardName; }
-	public String getMonsterType() { return this.monsterType; }
-	public String getCardColor() { return this.cardColor; }
-	public String getCardEffect() { return this.cardEffect; }
-	public String getCardID() { return this.cardID; }
-	public String getCardAttribute() { return this.cardAttribute; }
-	public int getMonsterAttack() {return this.monsterAttack; }
-	public int getMonsterDefense() { return this.monsterDefense; }
-
-
-
-	/**
-	 * Used to create a Card instance
-	 */
-	public static class Builder {
-		/** Name of the card */
-		private String cardName;
-		private String monsterType, cardColor, cardEffect, cardID, cardAttribute;
-		private int monsterAttack, monsterDefense;
-
-		/**
-		 * @param cardName The name of the card.
-		 * @return This Builder object.
-		 */
-		public Builder cardName(String cardName) {
-			this.cardName = cardName;
-			return this;
-		}
-
-		/**
-		 * @param monsterType The type of monster card.
-		 * @return This Builder object.
-		 */
-		public Builder monsterType(String monsterType) {
-			this.monsterType = monsterType;
-			return this;
-		}
-
-		/**
-		 * @param cardColor The color of the card.
-		 * @return This Builder object.
-		 */
-		public Builder cardColor(String cardColor) {
-			this.cardColor = cardColor;
-			return this;
-		}
-
-		/**
-		 * @param cardEffect The effect of the card.
-		 * @return This Builder object.
-		 */
-		public Builder cardEffect(String cardEffect) {
-			this.cardEffect = cardEffect;
-			return this;
-		}
-
-		/**
-		 * @param cardID The ID of the card (bottom of card).
-		 * @return This Builder object.
-		 */
-		public Builder cardID(String cardID) {
-			this.cardID = cardID;
-			return this;
-		}
-
-		/**
-		 * @param cardAttribute The attribute of the card.
-		 * @return This Builder object.
-		 */
-		public Builder cardAttribute(String cardAttribute) {
-			this.cardAttribute = cardAttribute;
-			return this;
-		}
-
-		/**
-		 * @param monsterAttack The attack of the card (if it is a monster card).
-		 * @return This Builder object.
-		 */
-		public Builder monsterAttack(int monsterAttack) {
-			this.monsterAttack = monsterAttack;
-			return this;
-		}
-
-		/**
-		 * @param monsterDefense The defense of the card (if it is a monster card).
-		 * @return This Builder object.
-		 */
-		public Builder monsterDefense(int monsterDefense) {
-			this.monsterDefense = monsterDefense;
-			return this;
-		}
-
-		/**
-		 * Uses this builder instance to create a new Card instance.
-		 *
-		 * @return New Card instance.
-		 */
-		public Card build() {
-			Card card = new Card();
-			// String
-			card.cardName = this.cardName;
-			card.monsterType = this.monsterType;
-			card.cardColor = this.cardColor;
-			card.cardEffect = this.cardEffect;
-			card.cardID = this.cardID;
-			card.cardAttribute = this.cardAttribute;
-
-			// Ints
-			card.monsterAttack = this.monsterAttack;
-			card.monsterDefense = this.monsterDefense;
-			return card;
-		}
 	}
 }
