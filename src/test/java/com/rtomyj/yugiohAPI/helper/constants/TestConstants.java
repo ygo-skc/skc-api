7dd7ed6e4c209968ff64d40d7331a26a03b9f68c
Javi Gomez <rtomyj@gmail.com>
2020-02-29 00:17:13 -0600

    Updated tests to use JSON file values.

--- src/test/java/com/rtomyj/yugiohAPI/helper/constants/TestConstants.java
+++ src/test/java/com/rtomyj/yugiohAPI/helper/constants/TestConstants.java
@@ -7,18 +7,22 @@ public class TestConstants
 	public static final String WRONG_HTTP_CODE_MESSAGE = "Incorrect HTTP Code";
 
 
+	public static final String BAN_LIST_INSTANCE_JSON_FILE = "src/test/resources/BanListInstanceTestResponse.json";
+	public static final String BAN_LIST_INSTANCE_LOW_BANDWIDTH_JSON_FILE = "src/test/resources/BanListInstanceLowBandwdithTestResponse.json";
+
+
 	public static final String STRATOS_ID = "40044918";
-	public static final String STRATOS_NAME = "40044918";
+	public static final String STRATOS_NAME = "Elemental HERO Stratos";
 	public static final String STRATOS_FULL_EFFECT = "When this card is Normal or Special Summoned: You can activate 1 of these effects.\n&bull; You can destroy Spell/Trap Cards on the field, up to the number of 'HERO' monsters you control, except this card.\n&bull; Add 1 'HERO' monster from your Deck to your hand.";
 	public static final String STRATOS_TRIMMED_EFFECT = "When this card is Normal or Special Summoned: You can activate 1 of these effects.\n&bull; You can destroy Spell/Trap Cards on the field, up to the number of 'HE...";
+
+	public static final String A_HERO_LIVES_ID = "08949584";
+	public static final String A_HERO_LIVES_NAME = "A Hero Lives";
+	public static final String A_HERO_LIVES_FULL_EFFECT = "If you control no face-up monsters: Pay half your LP; Special Summon 1 Level 4 or lower 'Elemental HERO' monster from your Deck.";
+	public static final String A_HERO_LIVES_TRIMMED_EFFECT = "If you control no face-up monsters: Pay half your LP; Special Summon 1 Level 4 or lower 'Elemental HERO' monster from your Deck.";
 
 	public static final String D_MALICIOUS_ID = "09411399";
 	public static final String D_MALICIOUS_NAME = "Destiny HERO - Malicious";
 	public static final String D_MALICIOUS_FULL_EFFECT = "You can banish this card from your GY; Special Summon 1 'Destiny HERO - Malicious' from your Deck.";
 	public static final String D_MALICIOUS_TRIMMED_EFFECT = "You can banish this card from your GY; Special Summon 1 'Destiny HERO - Malicious' from your Deck.";
-
-	public static final String A_HERO_LIVES_ID = "08949584";
-	public static final String A_HERO_LIVES_NAME = "A Hero Lives";
-	public static final String A_HERO_LIVES_FULL_EFFECT = "If you control no face-up monsters: Pay half your LP; Special Summon 1 Level 4 or lower 'Elemental HERO' monster from your Deck";
-	public static final String A_HERO_LIVES_TRIMMED_EFFECT = "If you control no face-up monsters: Pay half your LP; Special Summon 1 Level 4 or lower 'Elemental HERO' monster from your Deck";
 }
--- src/test/java/com/rtomyj/yugiohAPI/service/banlist/CardsServiceTest.java
+++ src/test/java/com/rtomyj/yugiohAPI/service/banlist/CardsServiceTest.java
@@ -10,10 +10,15 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rtomyj.yugiohAPI.configuration.exception.YgoException;
 import com.rtomyj.yugiohAPI.dao.database.Dao;
 import com.rtomyj.yugiohAPI.dao.database.Dao.Status;
@@ -31,11 +36,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
 
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CardsServiceTest
-{
+public class CardsServiceTest {
 	@InjectMocks
 	private CardsService cardsService;
 
@@ -43,94 +46,26 @@ public class CardsServiceTest
 	private Dao dao;
 
 	@Mock
-	private Map<String, BanListInstance>  BAN_LIST_CARDS_CACHE;
+	private Map<String, BanListInstance> BAN_LIST_CARDS_CACHE;
 
 	@Mock
-	private Map<String, BanListInstance>  BAN_LIST_CARDS_LOW_BANDWIDTH_CACHE;
+	private Map<String, BanListInstance> BAN_LIST_CARDS_LOW_BANDWIDTH_CACHE;
 
-
-	private BanListInstance cachedInstanceWithFullCardEffect;
-	private BanListInstance cachedInstanceWithTrimmedCardEffect;
+	private BanListInstance banListInstanceFullText;
+	private BanListInstance banListInstanceTrimmedText;
 
 	private final String banListStartDate = "2019-07-15";
 
-	private final List<Card> forbiddenFullEffect = new ArrayList<>();
-	private final List<Card> limitedFullEffect = new ArrayList<>();
-	private final List<Card> semiLimitedFullEffect = new ArrayList<>();
-
-	private final List<Card> forbiddenTrimmedEffect = new ArrayList<>();
-	private final List<Card> limitedTrimmedEffect = new ArrayList<>();
-	private final List<Card> semiLimitedTrimmedEffect = new ArrayList<>();
-
-	private Card forbiddenCardWithFullEffect;
-	private final String forbiddenCardId = TestConstants.A_HERO_LIVES_ID;
-	private final String forbiddenCardName = TestConstants.A_HERO_LIVES_NAME;
-	private final String forbiddenCardFullEffect = TestConstants.A_HERO_LIVES_FULL_EFFECT;
-	private final String forbiddenCardTrimmedEffect = TestConstants.A_HERO_LIVES_TRIMMED_EFFECT;
-
-	private Card limitedCardWithFullEffect;
-	private final String limitedCardId = TestConstants.STRATOS_ID;
-	private final String limitedCardName = TestConstants.STRATOS_NAME;
-	private final String limitedCardFullEffect = TestConstants.STRATOS_FULL_EFFECT;
-	private final String limitedCardTrimmedEffect = TestConstants.A_HERO_LIVES_NAME;
-
-	private Card semiLimitedCardWithFullEffect;
-	private final String semiLimitedCardId = TestConstants.D_MALICIOUS_ID;
-	private final String semiLimitedCardName = TestConstants.D_MALICIOUS_NAME;
-	private final String semiLimitedCardFullEffect = TestConstants.D_MALICIOUS_FULL_EFFECT;
-	private final String semiLimitedCardTrimmedEffect = TestConstants.D_MALICIOUS_TRIMMED_EFFECT;
 
 
 	@Before
-	public void before()
+	public void before() throws JsonParseException, JsonMappingException, IOException
 	{
-		this.forbiddenCardWithFullEffect = Card
-			.builder()
-			.cardID(forbiddenCardId)
-			.cardName(forbiddenCardName)
-			.cardEffect(forbiddenCardFullEffect)
-			.build();
-
-		this.limitedCardWithFullEffect = Card
-			.builder()
-			.cardID(limitedCardId)
-			.cardName(limitedCardName)
-			.cardEffect(limitedCardFullEffect)
-			.build();
-
-		this.semiLimitedCardWithFullEffect = Card
-			.builder()
-			.cardID(semiLimitedCardId)
-			.cardName(semiLimitedCardName)
-			.cardEffect(semiLimitedCardFullEffect)
-		.build();
-
-		this.forbiddenFullEffect.add(forbiddenCardWithFullEffect);
-		this.limitedFullEffect.add(limitedCardWithFullEffect);
-		this.semiLimitedFullEffect.add(semiLimitedCardWithFullEffect);
-
-		/*
-			Create forbidden, limited, and semi-limited card lists with trimmed card effects using previously defined
-			lists that contain full card effects as a base. The only field being changed will be the card effect as the trimmed
-			effect is needed. All other fields can stay the same.
-		*/
-		this.forbiddenTrimmedEffect.add(forbiddenCardWithFullEffect.withCardEffect(forbiddenCardTrimmedEffect));
-		this.limitedTrimmedEffect.add(limitedCardWithFullEffect.withCardEffect(limitedCardTrimmedEffect));
-		this.semiLimitedTrimmedEffect.add(semiLimitedCardWithFullEffect.withCardEffect(semiLimitedCardTrimmedEffect));
-
-		this.cachedInstanceWithFullCardEffect = BanListInstance
-			.builder()
-			.forbidden(forbiddenFullEffect)
-			.limited(limitedFullEffect)
-			.semiLimited(semiLimitedFullEffect)
-			.build();
-
-		this.cachedInstanceWithTrimmedCardEffect = BanListInstance
-			.builder()
-			.forbidden(forbiddenTrimmedEffect)
-			.limited(limitedTrimmedEffect)
-			.semiLimited(semiLimitedTrimmedEffect)
-			.build();
+		ObjectMapper mapper = new ObjectMapper();
+		this.banListInstanceFullText = mapper.readValue(
+			new File(TestConstants.BAN_LIST_INSTANCE_JSON_FILE), BanListInstance.class);
+		this.banListInstanceTrimmedText = mapper.readValue(
+			new File(TestConstants.BAN_LIST_INSTANCE_LOW_BANDWIDTH_JSON_FILE), BanListInstance.class);
 	}
 
 
@@ -138,21 +73,23 @@ public class CardsServiceTest
 	@Test
 	public void testFetchingBanListInstance_FromDB_WithFullText_Successfully() throws YgoException
 	{
+		final List<Card> forbidden = banListInstanceFullText.getForbidden();
+		final List<Card> limited = banListInstanceFullText.getLimited();
+		final List<Card> semiLimited = banListInstanceFullText.getSemiLimited();
+
+
 		when(dao.getBanListByBanStatus(eq(banListStartDate), eq(Status.FORBIDDEN)))
-			.thenReturn(forbiddenFullEffect);
+			.thenReturn(forbidden);
 		when(dao.getBanListByBanStatus(eq(banListStartDate), eq(Status.LIMITED)))
-			.thenReturn(limitedFullEffect);
+			.thenReturn(limited);
 		when(dao.getBanListByBanStatus(eq(banListStartDate), eq(Status.SEMI_LIMITED)))
-			.thenReturn(semiLimitedFullEffect);
+			.thenReturn(semiLimited);
 		when(BAN_LIST_CARDS_CACHE.get(eq(banListStartDate)))
 			.thenReturn(null);
 
 
 		final ServiceLayerHelper serviceLayerHelper = cardsService.getBanListByBanStatus(banListStartDate, false);
 		final BanListInstance banListInstance = (BanListInstance) serviceLayerHelper.getRequestedResource();
-		final List<Card> forbidden = banListInstance.getForbidden();
-		final List<Card> limited = banListInstance.getLimited();
-		final List<Card> semiLimited = banListInstance.getSemiLimited();
 
 		assertNotEquals(null, serviceLayerHelper);
 		assertNotEquals(null, banListInstance);
@@ -164,17 +101,17 @@ public class CardsServiceTest
 		assertEquals(1, limited.size());
 		assertEquals(1, semiLimited.size());
 
-		assertEquals(forbiddenCardId, forbidden.get(0).getCardID());
-		assertEquals(forbiddenCardName, forbidden.get(0).getCardName());
-		assertEquals(forbiddenCardFullEffect, forbidden.get(0).getCardEffect());
+		assertEquals(TestConstants.STRATOS_ID, forbidden.get(0).getCardID());
+		assertEquals(TestConstants.STRATOS_NAME, forbidden.get(0).getCardName());
+		assertEquals(TestConstants.STRATOS_FULL_EFFECT, forbidden.get(0).getCardEffect());
 
-		assertEquals(limitedCardId, limited.get(0).getCardID());
-		assertEquals(limitedCardName, limited.get(0).getCardName());
-		assertEquals(limitedCardFullEffect, limited.get(0).getCardEffect());
+		assertEquals(TestConstants.A_HERO_LIVES_ID, limited.get(0).getCardID());
+		assertEquals(TestConstants.A_HERO_LIVES_NAME, limited.get(0).getCardName());
+		assertEquals(TestConstants.A_HERO_LIVES_FULL_EFFECT, limited.get(0).getCardEffect());
 
-		assertEquals(semiLimitedCardId, semiLimited.get(0).getCardID());
-		assertEquals(semiLimitedCardName, semiLimited.get(0).getCardName());
-		assertEquals(semiLimitedCardFullEffect, semiLimited.get(0).getCardEffect());
+		assertEquals(TestConstants.D_MALICIOUS_ID, semiLimited.get(0).getCardID());
+		assertEquals(TestConstants.D_MALICIOUS_NAME, semiLimited.get(0).getCardName());
+		assertEquals(TestConstants.D_MALICIOUS_FULL_EFFECT, semiLimited.get(0).getCardEffect());
 
 		assertEquals(HttpStatus.OK, serviceLayerHelper.getStatus());
 		assertFalse(serviceLayerHelper.getInCache());
@@ -193,15 +130,17 @@ public class CardsServiceTest
 	@Test
 	public void testFetchingBanListInstance_FromCache_WithFullText_Successfully() throws YgoException
 	{
+		final List<Card> forbidden = banListInstanceFullText.getForbidden();
+		final List<Card> limited = banListInstanceFullText.getLimited();
+		final List<Card> semiLimited = banListInstanceFullText.getSemiLimited();
+
+
 		when(BAN_LIST_CARDS_CACHE.get(eq(banListStartDate)))
-			.thenReturn(cachedInstanceWithFullCardEffect);
+			.thenReturn(banListInstanceFullText);
 
 
 		final ServiceLayerHelper serviceLayerHelper = cardsService.getBanListByBanStatus(banListStartDate, false);
 		final BanListInstance banListInstance = (BanListInstance) serviceLayerHelper.getRequestedResource();
-		final List<Card> forbidden = banListInstance.getForbidden();
-		final List<Card> limited = banListInstance.getLimited();
-		final List<Card> semiLimited = banListInstance.getSemiLimited();
 
 		assertNotEquals(null, serviceLayerHelper);
 		assertNotEquals(null, banListInstance);
@@ -213,17 +152,17 @@ public class CardsServiceTest
 		assertEquals(1, limited.size());
 		assertEquals(1, semiLimited.size());
 
-		assertEquals(forbiddenCardId, forbidden.get(0).getCardID());
-		assertEquals(forbiddenCardName, forbidden.get(0).getCardName());
-		assertEquals(forbiddenCardFullEffect, forbidden.get(0).getCardEffect());
+		assertEquals(TestConstants.STRATOS_ID, forbidden.get(0).getCardID());
+		assertEquals(TestConstants.STRATOS_NAME, forbidden.get(0).getCardName());
+		assertEquals(TestConstants.STRATOS_FULL_EFFECT, forbidden.get(0).getCardEffect());
 
-		assertEquals(limitedCardId, limited.get(0).getCardID());
-		assertEquals(limitedCardName, limited.get(0).getCardName());
-		assertEquals(limitedCardFullEffect, limited.get(0).getCardEffect());
+		assertEquals(TestConstants.A_HERO_LIVES_ID, limited.get(0).getCardID());
+		assertEquals(TestConstants.A_HERO_LIVES_NAME, limited.get(0).getCardName());
+		assertEquals(TestConstants.A_HERO_LIVES_FULL_EFFECT, limited.get(0).getCardEffect());
 
-		assertEquals(semiLimitedCardId, semiLimited.get(0).getCardID());
-		assertEquals(semiLimitedCardName, semiLimited.get(0).getCardName());
-		assertEquals(semiLimitedCardFullEffect, semiLimited.get(0).getCardEffect());
+		assertEquals(TestConstants.D_MALICIOUS_ID, semiLimited.get(0).getCardID());
+		assertEquals(TestConstants.D_MALICIOUS_NAME, semiLimited.get(0).getCardName());
+		assertEquals(TestConstants.D_MALICIOUS_FULL_EFFECT, semiLimited.get(0).getCardEffect());
 
 		assertEquals(HttpStatus.OK, serviceLayerHelper.getStatus());
 		assertTrue(serviceLayerHelper.getInCache());
@@ -242,43 +181,49 @@ public class CardsServiceTest
 	@Test
 	public void testFetchingBanListInstance_FromDB_WithTrimmedText_Successfully() throws YgoException
 	{
+		final List<Card> forbiddenFull = banListInstanceFullText.getForbidden();
+		final List<Card> limitedFull = banListInstanceFullText.getLimited();
+		final List<Card> semiLimitedFull = banListInstanceFullText.getSemiLimited();
+
+
 		when(dao.getBanListByBanStatus(eq(banListStartDate), eq(Status.FORBIDDEN)))
-			.thenReturn(forbiddenTrimmedEffect);
+			.thenReturn(forbiddenFull);
 		when(dao.getBanListByBanStatus(eq(banListStartDate), eq(Status.LIMITED)))
-			.thenReturn(limitedTrimmedEffect);
+			.thenReturn(limitedFull);
 		when(dao.getBanListByBanStatus(eq(banListStartDate), eq(Status.SEMI_LIMITED)))
-			.thenReturn(semiLimitedTrimmedEffect);
+			.thenReturn(semiLimitedFull);
 		when(BAN_LIST_CARDS_LOW_BANDWIDTH_CACHE.get(eq(banListStartDate)))
 			.thenReturn(null);
 
 
 		final ServiceLayerHelper serviceLayerHelper = cardsService.getBanListByBanStatus(banListStartDate, true);
 		final BanListInstance banListInstance = (BanListInstance) serviceLayerHelper.getRequestedResource();
-		final List<Card> forbidden = banListInstance.getForbidden();
-		final List<Card> limited = banListInstance.getLimited();
-		final List<Card> semiLimited = banListInstance.getSemiLimited();
+
+		final List<Card> forbiddenTrimmed = banListInstanceTrimmedText.getForbidden();
+		final List<Card> limitedTrimmed = banListInstanceTrimmedText.getLimited();
+		final List<Card> semiLimitedTrimmed = banListInstanceTrimmedText.getSemiLimited();
 
 		assertNotEquals(null, serviceLayerHelper);
 		assertNotEquals(null, banListInstance);
-		assertNotEquals(null, forbidden);
-		assertNotEquals(null, limited);
-		assertNotEquals(null, semiLimited);
+		assertNotEquals(null, forbiddenTrimmed);
+		assertNotEquals(null, limitedTrimmed);
+		assertNotEquals(null, semiLimitedTrimmed);
 
-		assertEquals(1, forbidden.size());
-		assertEquals(1, limited.size());
-		assertEquals(1, semiLimited.size());
+		assertEquals(1, forbiddenTrimmed.size());
+		assertEquals(1, limitedTrimmed.size());
+		assertEquals(1, semiLimitedTrimmed.size());
 
-		assertEquals(forbiddenCardId, forbidden.get(0).getCardID());
-		assertEquals(forbiddenCardName, forbidden.get(0).getCardName());
-		assertEquals(forbiddenCardTrimmedEffect, forbidden.get(0).getCardEffect());
+		assertEquals(TestConstants.STRATOS_ID, forbiddenTrimmed.get(0).getCardID());
+		assertEquals(TestConstants.STRATOS_NAME, forbiddenTrimmed.get(0).getCardName());
+		assertEquals(TestConstants.STRATOS_TRIMMED_EFFECT, forbiddenTrimmed.get(0).getCardEffect());
 
-		assertEquals(limitedCardId, limited.get(0).getCardID());
-		assertEquals(limitedCardName, limited.get(0).getCardName());
-		assertEquals(limitedCardTrimmedEffect, limited.get(0).getCardEffect());
+		assertEquals(TestConstants.A_HERO_LIVES_ID, limitedTrimmed.get(0).getCardID());
+		assertEquals(TestConstants.A_HERO_LIVES_NAME, limitedTrimmed.get(0).getCardName());
+		assertEquals(TestConstants.A_HERO_LIVES_TRIMMED_EFFECT, limitedTrimmed.get(0).getCardEffect());
 
-		assertEquals(semiLimitedCardId, semiLimited.get(0).getCardID());
-		assertEquals(semiLimitedCardName, semiLimited.get(0).getCardName());
-		assertEquals(semiLimitedCardTrimmedEffect, semiLimited.get(0).getCardEffect());
+		assertEquals(TestConstants.D_MALICIOUS_ID, semiLimitedTrimmed.get(0).getCardID());
+		assertEquals(TestConstants.D_MALICIOUS_NAME, semiLimitedTrimmed.get(0).getCardName());
+		assertEquals(TestConstants.D_MALICIOUS_TRIMMED_EFFECT, semiLimitedTrimmed.get(0).getCardEffect());
 
 		assertEquals(HttpStatus.OK, serviceLayerHelper.getStatus());
 		assertFalse(serviceLayerHelper.getInCache());
@@ -298,36 +243,37 @@ public class CardsServiceTest
 	public void testFetchingBanListInstance_FromCache_WithTrimmedText_Successfully() throws YgoException
 	{
 		when(BAN_LIST_CARDS_LOW_BANDWIDTH_CACHE.get(eq(banListStartDate)))
-			.thenReturn(cachedInstanceWithTrimmedCardEffect);
+			.thenReturn(banListInstanceTrimmedText);
 
 
 		final ServiceLayerHelper serviceLayerHelper = cardsService.getBanListByBanStatus(banListStartDate, true);
 		final BanListInstance banListInstance = (BanListInstance) serviceLayerHelper.getRequestedResource();
-		final List<Card> forbidden = banListInstance.getForbidden();
-		final List<Card> limited = banListInstance.getLimited();
-		final List<Card> semiLimited = banListInstance.getSemiLimited();
+
+		final List<Card> forbiddenTrimmed = banListInstanceTrimmedText.getForbidden();
+		final List<Card> limitedTrimmed = banListInstanceTrimmedText.getLimited();
+		final List<Card> semiLimitedTrimmed = banListInstanceTrimmedText.getSemiLimited();
 
 		assertNotEquals(null, serviceLayerHelper);
 		assertNotEquals(null, banListInstance);
-		assertNotEquals(null, forbidden);
-		assertNotEquals(null, limited);
-		assertNotEquals(null, semiLimited);
+		assertNotEquals(null, forbiddenTrimmed);
+		assertNotEquals(null, limitedTrimmed);
+		assertNotEquals(null, semiLimitedTrimmed);
 
-		assertEquals(1, forbidden.size());
-		assertEquals(1, limited.size());
-		assertEquals(1, semiLimited.size());
+		assertEquals(1, forbiddenTrimmed.size());
+		assertEquals(1, limitedTrimmed.size());
+		assertEquals(1, semiLimitedTrimmed.size());
 
-		assertEquals(forbiddenCardId, forbidden.get(0).getCardID());
-		assertEquals(forbiddenCardName, forbidden.get(0).getCardName());
-		assertEquals(forbiddenCardTrimmedEffect, forbidden.get(0).getCardEffect());
+		assertEquals(TestConstants.STRATOS_ID, forbiddenTrimmed.get(0).getCardID());
+		assertEquals(TestConstants.STRATOS_NAME, forbiddenTrimmed.get(0).getCardName());
+		assertEquals(TestConstants.STRATOS_TRIMMED_EFFECT, forbiddenTrimmed.get(0).getCardEffect());
 
-		assertEquals(limitedCardId, limited.get(0).getCardID());
-		assertEquals(limitedCardName, limited.get(0).getCardName());
-		assertEquals(limitedCardTrimmedEffect, limited.get(0).getCardEffect());
+		assertEquals(TestConstants.A_HERO_LIVES_ID, limitedTrimmed.get(0).getCardID());
+		assertEquals(TestConstants.A_HERO_LIVES_NAME, limitedTrimmed.get(0).getCardName());
+		assertEquals(TestConstants.A_HERO_LIVES_TRIMMED_EFFECT, limitedTrimmed.get(0).getCardEffect());
 
-		assertEquals(semiLimitedCardId, semiLimited.get(0).getCardID());
-		assertEquals(semiLimitedCardName, semiLimited.get(0).getCardName());
-		assertEquals(semiLimitedCardTrimmedEffect, semiLimited.get(0).getCardEffect());
+		assertEquals(TestConstants.D_MALICIOUS_ID, semiLimitedTrimmed.get(0).getCardID());
+		assertEquals(TestConstants.D_MALICIOUS_NAME, semiLimitedTrimmed.get(0).getCardName());
+		assertEquals(TestConstants.D_MALICIOUS_TRIMMED_EFFECT, semiLimitedTrimmed.get(0).getCardEffect());
 
 		assertEquals(HttpStatus.OK, serviceLayerHelper.getStatus());
 		assertTrue(serviceLayerHelper.getInCache());
