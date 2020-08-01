efe1793ecc00127256cdabbce7fa9037e5c2d4c5
Javi Gomez <rtomyj@gmail.com>
2020-08-01 15:25:34 -0500

    Helper for MDC configuration of basic info

---
+++ src/main/java/com/rtomyj/yugiohAPI/helper/Logging.java
@@ -0,0 +1,18 @@
+package com.rtomyj.yugiohAPI.helper;
+
+import org.slf4j.MDC;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class Logging
+{
+
+    public static void configureMDC(final HttpServletRequest httpServletRequest, final String endpoint)
+    {
+
+        MDC.put("reqIp", httpServletRequest.getRemoteHost());
+        MDC.put("reqRes", endpoint);
+        
+    }
+
+}
