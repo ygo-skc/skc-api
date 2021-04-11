959cbd9ff883c17930369e8d280b3f2a3ecfebb3
Javi Gomez <rtomyj@gmail.com>
2021-04-11 09:24:01 -0500

    Restructering

---
+++ src/perfTest/scala/com/rtomyj/skc/constants/Url.scala
@@ -0,0 +1,15 @@
+package com.rtomyj.skc.constants
+
+object Url
+{
+  // hosts
+  val localhost = "http://localhost:9999"
+  val localhostDocker = "http://localhost:9998"
+  val dev = "https://dev.skc-ygo-api.com"
+  val prod = "https://skc-ygo-api.com"
+
+
+  // endpoints
+  val browseCardsEndpoint = "/api/v1/card/browse"
+  val cardBrowseCriteriaEndpoint = "/api/v1/card/browse/criteria"
+}
