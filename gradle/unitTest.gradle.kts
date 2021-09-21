a0f649c8df3aa927939c84022e06ea13a3ac8183
Javi <rtomyj@gmail.com>
2021-09-20 21:27:04 -0500

    Removed explicit JUnit inclusion as Kotlin test includes lib.

--- gradle/unitTest.gradle.kts
+++ gradle/unitTest.gradle.kts
@@ -1,5 +1,4 @@
 val springVersion = "2.5.4"
-val junitVersion = "5.8.0"
 val h2Version = "1.4.200"
 
 dependencies {
@@ -7,9 +6,6 @@ dependencies {
 
     "testImplementation"("org.springframework.boot:spring-boot-starter-test:$springVersion")
     "testRuntimeOnly"("com.h2database:h2:$h2Version")
-
-    "testImplementation"("org.junit.jupiter:junit-jupiter-api:$junitVersion")
-    "testImplementation"("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
 }
 
 
