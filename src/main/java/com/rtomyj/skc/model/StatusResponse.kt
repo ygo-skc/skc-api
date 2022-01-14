469b95bb37ba308bcb211f999a8f20f2ba8dcb1e
Javi <rtomyj@gmail.com>
2022-01-13 18:29:49 -0600

    Java to KT

---
+++ src/main/java/com/rtomyj/skc/model/StatusResponse.kt
@@ -0,0 +1,12 @@
+package com.rtomyj.skc.model
+
+import io.swagger.annotations.ApiModel
+import io.swagger.annotations.ApiModelProperty
+
+@ApiModel(description = "Return object for test call endpoint.")
+class StatusResponse(
+	@ApiModelProperty(value = "Current status of API.", accessMode = ApiModelProperty.AccessMode.READ_ONLY)
+			val status: String,
+	@ApiModelProperty(value = "Current API version.", accessMode = ApiModelProperty.AccessMode.READ_ONLY)
+			val version: String
+)
