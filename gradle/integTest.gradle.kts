257f3d7f68d10eda05781193e19c5ef512aed7b8
Javi <rtomyj@gmail.com>
2022-03-30 23:01:27 -0400

    Updating groovy versions

--- gradle/integTest.gradle.kts
+++ gradle/integTest.gradle.kts
@@ -1,6 +1,6 @@
 val cucumberVersion = "7.2.3"
 val restAssuredVersion = "5.0.0"
-val groovyVersion = "3.0.10"
+val groovyVersion = "4.0.1"
 val junitVersion = "5.8.2"
 val commonsLoggingVersion = "1.2"
 
@@ -15,8 +15,8 @@ dependencies {
     "integTestImplementation"("io.rest-assured:json-schema-validator:${restAssuredVersion}")
     "integTestImplementation"("io.rest-assured:rest-assured-common:${restAssuredVersion}")
 
-    "integTestImplementation"("org.codehaus.groovy:groovy:${groovyVersion}")   // Need to specify groovy version >= 3 to be able to use rest assured version >= 4.3
-    "integTestImplementation"("org.codehaus.groovy:groovy-xml:${groovyVersion}")   // Need to specify groovy version >= 3 to be able to use rest assured version >= 4.3
+    "integTestImplementation"("org.apache.groovy:groovy:${groovyVersion}")   // Need to specify groovy version >= 3 to be able to use rest assured version >= 4.3
+    "integTestImplementation"("org.apache.groovy:groovy-xml:${groovyVersion}")   // Need to specify groovy version >= 3 to be able to use rest assured version >= 4.3
 
     "integTestImplementation"("org.junit.jupiter:junit-jupiter-api:$junitVersion")
     "integTestImplementation"("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
