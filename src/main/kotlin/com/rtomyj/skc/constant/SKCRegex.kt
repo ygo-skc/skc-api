8da6f5a47e62bcc6864377336a894aa86c4b1f09
Javi <rtomyj@gmail.com>
2022-05-17 17:04:12 -0500

    Updated REGEX

--- src/main/kotlin/com/rtomyj/skc/constant/SKCRegex.kt
+++ src/main/kotlin/com/rtomyj/skc/constant/SKCRegex.kt
@@ -3,7 +3,7 @@ package com.rtomyj.skc.constant
 object SKCRegex {
 	const val DB_DATE = "[0-9]{4}-[0-9]{2}-[0-9]{2}"
 	const val CARD_ID = "[0-9]{8}"
-	const val PRODUCT_ID= "[a-zA-Z0-9]{3,4}"
+	const val PRODUCT_ID = "[a-zA-Z0-9]{3,4}(-SE)?" // TODO: update db to handle Special Editions more elegantly
 	const val PRODUCT_TYPE = "[a-zA-Z]+"
 	const val LOCALE = "[a-zA-Z]{2}"
 }
