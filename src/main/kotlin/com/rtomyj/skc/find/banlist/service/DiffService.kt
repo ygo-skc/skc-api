72a2f3113ca1c59bf309e039b324c3428311c90f
Javi <rtomyj@gmail.com>
2022-10-19 16:58:19 -0500

    Fixed issue occurring when user wants new ban list inclusions for DL format.

--- src/main/kotlin/com/rtomyj/skc/find/banlist/service/DiffService.kt
+++ src/main/kotlin/com/rtomyj/skc/find/banlist/service/DiffService.kt
@@ -22,6 +22,50 @@ class DiffService @Autowired constructor(
 		private val log = LoggerFactory.getLogger(this::class.java.name)
 	}
 
+	fun newContentNormalFormat(banListStartDate: String, format: String): BanListNewContent {
+		val forbidden = banListDao.getNewContentOfBanList(banListStartDate, BanListCardStatus.FORBIDDEN, format)
+		val limited = banListDao.getNewContentOfBanList(banListStartDate, BanListCardStatus.LIMITED, format)
+		val semiLimited = banListDao.getNewContentOfBanList(banListStartDate, BanListCardStatus.SEMI_LIMITED, format)
+
+		forbidden.forEach { MonsterAssociation.transformMonsterLinkRating(it.card) }
+		limited.forEach { MonsterAssociation.transformMonsterLinkRating(it.card) }
+		semiLimited.forEach { MonsterAssociation.transformMonsterLinkRating(it.card) }
+
+		return BanListNewContent(
+			banListStartDate,
+			getPreviousBanListDate(banListStartDate, format),
+			forbidden,
+			limited,
+			semiLimited,
+			emptyList(),
+			emptyList(),
+			emptyList()
+		)
+	}
+
+	fun newContentDuelLinksFormat(banListStartDate: String, format: String): BanListNewContent {
+		val forbidden = banListDao.getNewContentOfBanList(banListStartDate, BanListCardStatus.FORBIDDEN, format)
+		val limitedOne = banListDao.getNewContentOfBanList(banListStartDate, BanListCardStatus.LIMITED_ONE, format)
+		val limitedTwo = banListDao.getNewContentOfBanList(banListStartDate, BanListCardStatus.LIMITED_TWO, format)
+		val limitedThree = banListDao.getNewContentOfBanList(banListStartDate, BanListCardStatus.LIMITED_THREE, format)
+
+		forbidden.forEach { MonsterAssociation.transformMonsterLinkRating(it.card) }
+		limitedOne.forEach { MonsterAssociation.transformMonsterLinkRating(it.card) }
+		limitedTwo.forEach { MonsterAssociation.transformMonsterLinkRating(it.card) }
+		limitedThree.forEach { MonsterAssociation.transformMonsterLinkRating(it.card) }
+
+		return BanListNewContent(
+			banListStartDate,
+			getPreviousBanListDate(banListStartDate, format),
+			forbidden,
+			emptyList(),
+			emptyList(),
+			limitedOne,
+			limitedTwo,
+			limitedThree
+		)
+	}
+
 
 	@Throws(SKCException::class)
 	fun getNewContentForGivenBanList(banListStartDate: String, format: String): BanListNewContent {
@@ -36,21 +80,7 @@ class DiffService @Autowired constructor(
 
 
 		// builds meta data object for new cards request
-		val forbidden = banListDao.getNewContentOfBanList(banListStartDate, BanListCardStatus.FORBIDDEN, format)
-		val limited = banListDao.getNewContentOfBanList(banListStartDate, BanListCardStatus.LIMITED, format)
-		val semiLimited = banListDao.getNewContentOfBanList(banListStartDate, BanListCardStatus.SEMI_LIMITED, format)
-
-		forbidden.forEach { MonsterAssociation.transformMonsterLinkRating(it.card) }
-		limited.forEach { MonsterAssociation.transformMonsterLinkRating(it.card) }
-		semiLimited.forEach { MonsterAssociation.transformMonsterLinkRating(it.card) }
-
-		val newCardsMeta = BanListNewContent(
-			banListStartDate,
-			getPreviousBanListDate(banListStartDate, format),
-			forbidden,
-			limited,
-			semiLimited,
-		)
+		val newCardsMeta = if (format == "DL") newContentDuelLinksFormat(banListStartDate, format) else newContentNormalFormat(banListStartDate, format)
 
 		newCardsMeta.setLinks()
 		return newCardsMeta
