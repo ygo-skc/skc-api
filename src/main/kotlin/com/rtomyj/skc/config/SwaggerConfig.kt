b4c215996f9b6a7f06ca185ab00424905a411a3d
Javi <rtomyj@gmail.com>
2023-01-08 17:09:54 -0600

    Cleanup
    Signed-off-by: Javi <rtomyj@gmail.com>

--- src/main/kotlin/com/rtomyj/skc/config/SwaggerConfig.kt
+++ src/main/kotlin/com/rtomyj/skc/config/SwaggerConfig.kt
@@ -1,5 +1,6 @@
 package com.rtomyj.skc.config
 
+import com.rtomyj.skc.util.constant.AppConstants
 import io.swagger.v3.oas.models.ExternalDocumentation
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
@@ -18,15 +19,16 @@ class SwaggerConfig {
 	 * @return info about me. Will be used by swagger to display contact information and licenses.
 	 */
 	@Bean
-	public fun infoSection(): OpenAPI {
+	fun infoSection(): OpenAPI {
 		return OpenAPI()
 			.info(
 				Info()
 					.title("SKC API")
 					.description("Application Programming Interface (or API for short) for interfacing with a Database that contains information such as Ban List dates/content, Yu-Gi-Oh! product information, card information, etc.")
-					.version("v1.3.0")
+					.version(AppConstants.APP_VERSION)
 					.license(License().name("Apache License Version 2.0").url("https://www.apache.org/licenses/LICENSE-2.0"))
-					.termsOfService("https://github.com/ygo-skc/skc-api#readme"))
+					.termsOfService("https://github.com/ygo-skc/skc-api#readme")
+			)
 			.externalDocs(
 				ExternalDocumentation()
 					.description("GitHub")
