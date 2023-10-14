9e2c383cce6c1715fcdbb1ae4b5f38a8e96d23da
Javi <8519394+rtomyj@users.noreply.github.com>
2023-10-14 12:30:32 -0500

    Updating script to move jks to appropriate dir on completion
    Signed-off-by: Javi <8519394+rtomyj@users.noreply.github.com>

--- doppler-secrets-local-setup.sh
+++ doppler-secrets-local-setup.sh
@@ -12,8 +12,11 @@ export PK_PASSWORD
 export SSL_KEYSTORE_PASSWORD
 
 # Create keystore w/ certs
-cd certs
+cd certs || exit
 openssl pkcs12 -export -name skcapi -in certificate.crt -inkey private.key -out skc-api.p12 -password "pass:$PK_PASSWORD"
 keytool -importkeystore -deststorepass "$SSL_KEYSTORE_PASSWORD" -destkeystore skc-api.jks \
  -srckeystore skc-api.p12 -srcstoretype PKCS12 -srcstorepass "$PK_PASSWORD"
- keytool -import -alias skc-api -trustcacerts -file ca_bundle.crt -keystore skc-api.jks -storepass "$SSL_KEYSTORE_PASSWORD"
+keytool -import -alias skc-api -trustcacerts -file ca_bundle.crt -keystore skc-api.jks -storepass "$SSL_KEYSTORE_PASSWORD"
+
+cd ..
+mv certs/skc-api.jks src/main/resources/skc-api.jks
