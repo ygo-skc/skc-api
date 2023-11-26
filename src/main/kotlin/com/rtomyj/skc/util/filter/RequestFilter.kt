b3b27bd416c00b8b0ebd51cfff029916efebc67a
Javi <8519394+rtomyj@users.noreply.github.com>
2023-11-26 16:31:05 -0600

    Migrating filter for use in webflux world

--- src/main/kotlin/com/rtomyj/skc/util/filter/RequestFilter.kt
+++ src/main/kotlin/com/rtomyj/skc/util/filter/RequestFilter.kt
@@ -1,46 +1,49 @@
 package com.rtomyj.skc.util.filter
 
+import com.google.common.base.Strings
+import com.google.common.net.HttpHeaders.USER_AGENT
 import com.rtomyj.skc.util.constant.AppConstants.CLIENT_IP_MDC
-import jakarta.servlet.FilterChain
-import jakarta.servlet.http.HttpServletRequest
-import jakarta.servlet.http.HttpServletResponse
 import org.slf4j.MDC
+import org.springframework.http.server.reactive.ServerHttpRequest
 import org.springframework.stereotype.Component
 import org.springframework.util.StringUtils
-import org.springframework.web.filter.OncePerRequestFilter
+import org.springframework.web.server.ServerWebExchange
+import org.springframework.web.server.WebFilter
+import org.springframework.web.server.WebFilterChain
+import reactor.core.publisher.Mono
 import java.util.*
 
 @Component
-class RequestFilter : OncePerRequestFilter() {
-	companion object {
-		private const val X_FORWARDED_FOR = "X-Forwarded-For"
-		const val CLIENT_ID_NAME = "CLIENT_ID"
+class RequestFilter : WebFilter {
+    companion object {
+        private const val X_FORWARDED_FOR = "X-Forwarded-For"
+        const val CLIENT_ID_NAME = "CLIENT_ID"
 
-		/**
-		 * Configures the global MDC object for all requests. MDC is used to hold useful info that will later be used in logs to better track requests.
-		 * @param httpServletRequest Contains useful information about new requests to the server from the client that will be used to access IP address and header information.
-		 */
-		fun configureMDC(httpServletRequest: HttpServletRequest, clientIP: String) {
-			val queryParams = if (httpServletRequest.queryString == null) "" else "?" + httpServletRequest.queryString
+        /**
+         * Configures the global MDC object for all requests. MDC is used to hold useful info that will later be used in logs to better track requests.
+         * @param request Contains useful information about new requests to the server from the client that will be used to access IP address and header information.
+         */
+        fun configureMDC(request: ServerHttpRequest) {
+            val headers = request.headers
+            val clientIP =
+                if (StringUtils.hasLength(headers.getFirst(X_FORWARDED_FOR))) headers.getFirst(X_FORWARDED_FOR) else request.remoteAddress?.address?.hostAddress
+                    ?: ""
+            val queryParams = if (Strings.isNullOrEmpty(request.uri.query)) "" else "?" + request.uri.query
 
-			// proxies and load balancers will forward client IP address in HTTP_X_FORWARDED_FOR header. If header exists, use value. Otherwise, use requests IP
-			MDC.put(CLIENT_IP_MDC, clientIP.replace("[", "").replace("]", ""))
-			MDC.put("reqPath", httpServletRequest.servletPath + queryParams)
-			MDC.put("reqUUID", UUID.randomUUID().toString())
-			MDC.put("clientID", httpServletRequest.getHeader(CLIENT_ID_NAME))
-			MDC.put("userAgent", httpServletRequest.getHeader(org.springframework.http.HttpHeaders.USER_AGENT))
-		}
-	}
+            // proxies and load balancers will forward client IP address in HTTP_X_FORWARDED_FOR header. If header exists, use value. Otherwise, use requests IP
+            MDC.put(CLIENT_IP_MDC, clientIP.replace("[", "").replace("]", ""))
+            MDC.put("reqPath", request.uri.path + queryParams)
+            MDC.put("reqUUID", UUID.randomUUID().toString())
+            MDC.put("clientID", headers.getFirst(CLIENT_ID_NAME))
+            MDC.put("userAgent", headers.getFirst(USER_AGENT))
+        }
+    }
 
-	override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, chain: FilterChain) {
-		try {
-			val clientIP =
-				if (StringUtils.hasLength(request.getHeader(X_FORWARDED_FOR))) request.getHeader(X_FORWARDED_FOR) else request.remoteHost
-
-			configureMDC(request, clientIP)
-			chain.doFilter(request, response)
-		} finally {
-			MDC.clear()
-		}
-	}
+    override fun filter(
+        serverWebExchange: ServerWebExchange, chain: WebFilterChain
+    ): Mono<Void> = chain.filter(serverWebExchange).doOnSubscribe {
+        configureMDC(serverWebExchange.request)
+    }.doFinally {
+        MDC.clear()
+    }
 }
--- src/test/kotlin/com/rtomyj/skc/util/filter/RequestFilterTest.kt
+++ src/test/kotlin/com/rtomyj/skc/util/filter/RequestFilterTest.kt
@@ -1,43 +1,48 @@
 package com.rtomyj.skc.util.filter
 
-import com.nhaarman.mockito_kotlin.atLeastOnce
-import jakarta.servlet.http.HttpServletRequest
+import com.google.common.net.HttpHeaders.USER_AGENT
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.slf4j.MDC
 import org.springframework.http.HttpHeaders
+import org.springframework.http.server.reactive.ServerHttpRequest
 import org.springframework.test.context.junit.jupiter.SpringExtension
+import java.net.URI
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 
 @ExtendWith(SpringExtension::class)
 class RequestFilterTest {
     @Mock
-    private lateinit var httpServletRequest: HttpServletRequest
+    private lateinit var request: ServerHttpRequest
 
     companion object {
-        private const val path = "/path"
-        private const val queryString = "x=y"
-        private const val clientID = "JUNIT"
-        private const val agent = "POSTMAN"
+        private const val PATH = "/path"
+        private const val QUERY_STRING = "x=y"
+        private const val CLIENT_ID = "JUNIT"
+        private const val AGENT = "POSTMAN"
+        private val HEADERS = HttpHeaders()
+
+        @BeforeAll
+        @JvmStatic
+        fun beforeAll() {
+            HEADERS.add("X-Forwarded-For", "1.1.1.1")
+            HEADERS.add("CLIENT_ID", CLIENT_ID)
+            HEADERS.add(USER_AGENT, AGENT)
+        }
     }
 
     @Test
     fun `Test MDC Configuration - Query String Not Null`() {
         // mock
-        Mockito.`when`(httpServletRequest.servletPath)
-            .thenReturn(path)
-        Mockito.`when`(httpServletRequest.queryString)
-            .thenReturn(queryString)
-        Mockito.`when`(httpServletRequest.getHeader(RequestFilter.CLIENT_ID_NAME))
-            .thenReturn(clientID)
-        Mockito.`when`(httpServletRequest.getHeader(HttpHeaders.USER_AGENT))
-            .thenReturn(agent)
+        Mockito.`when`(request.uri).thenReturn(URI("http", "", PATH, QUERY_STRING, ""))
+        Mockito.`when`(request.headers).thenReturn(HEADERS)
 
         // call
-        RequestFilter.configureMDC(httpServletRequest, "")
+        RequestFilter.configureMDC(request)
 
         // verify
         assertNotNull(MDC.get("reqPath"))
@@ -45,34 +50,19 @@ class RequestFilterTest {
         assertNotNull(MDC.get("clientID"))
         assertNotNull(MDC.get("userAgent"))
 
-        assertEquals("$path?$queryString", MDC.get("reqPath"))
-        assertEquals(clientID, MDC.get("clientID"))
-        assertEquals(agent, MDC.get("userAgent"))
-
-        Mockito.verify(httpServletRequest)
-            .servletPath
-        Mockito.verify(httpServletRequest, atLeastOnce())
-            .queryString
-        Mockito.verify(httpServletRequest)
-            .getHeader(RequestFilter.CLIENT_ID_NAME)
-        Mockito.verify(httpServletRequest)
-            .getHeader(HttpHeaders.USER_AGENT)
+        assertEquals("$PATH?$QUERY_STRING", MDC.get("reqPath"))
+        assertEquals(CLIENT_ID, MDC.get("clientID"))
+        assertEquals(AGENT, MDC.get("userAgent"))
     }
 
     @Test
     fun `Test MDC Configuration - Query String Is Null`() {
         // mock
-        Mockito.`when`(httpServletRequest.servletPath)
-            .thenReturn(path)
-        Mockito.`when`(httpServletRequest.queryString)
-            .thenReturn(null)
-        Mockito.`when`(httpServletRequest.getHeader(RequestFilter.CLIENT_ID_NAME))
-            .thenReturn(clientID)
-        Mockito.`when`(httpServletRequest.getHeader(HttpHeaders.USER_AGENT))
-            .thenReturn(agent)
+        Mockito.`when`(request.uri).thenReturn(URI("http", "", PATH, "", ""))
+        Mockito.`when`(request.headers).thenReturn(HEADERS)
 
         // call
-        RequestFilter.configureMDC(httpServletRequest, "")
+        RequestFilter.configureMDC(request)
 
         // verify
         assertNotNull(MDC.get("reqPath"))
@@ -80,17 +70,8 @@ class RequestFilterTest {
         assertNotNull(MDC.get("clientID"))
         assertNotNull(MDC.get("userAgent"))
 
-        assertEquals(path, MDC.get("reqPath"))
-        assertEquals(clientID, MDC.get("clientID"))
-        assertEquals(agent, MDC.get("userAgent"))
-
-        Mockito.verify(httpServletRequest)
-            .servletPath
-        Mockito.verify(httpServletRequest)
-            .queryString
-        Mockito.verify(httpServletRequest)
-            .getHeader(RequestFilter.CLIENT_ID_NAME)
-        Mockito.verify(httpServletRequest)
-            .getHeader(HttpHeaders.USER_AGENT)
+        assertEquals(PATH, MDC.get("reqPath"))
+        assertEquals(CLIENT_ID, MDC.get("clientID"))
+        assertEquals(AGENT, MDC.get("userAgent"))
     }
 }
