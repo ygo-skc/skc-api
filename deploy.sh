f13a0da1e4d9ee7f771533f3363de5a6b4162fd9
Javi <8519394+rtomyj@users.noreply.github.com>
2024-03-27 18:12:33 -0500

    Updated config

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,12 @@ fi
 
 SERVER=$1
 USER="ec2-user"
-DIR_ON_SERVER="skc-api"
+DIR_ON_SERVER="api/skc-api"
 
 echo "Using server $SERVER and directory $DIR_ON_SERVER to sync prod API"
 
 echo "Uploading API files"
-rsync -avz -e "ssh -i ~/.ssh/skc-server.pem" docker-compose.yml "${USER}@${SERVER}:${DIR_ON_SERVER}/"
+rsync --rsync-path="mkdir -p ${DIR_ON_SERVER} && rsync" -avz -e "ssh -i ~/.ssh/skc-server.pem" docker-compose.yml "${USER}@${SERVER}:${DIR_ON_SERVER}/"
 rsync -avz -e "ssh -i ~/.ssh/skc-server.pem" -R "build/libs/skc-api.jar" "${USER}@${SERVER}:${DIR_ON_SERVER}/"
 
 echo "Restaging API"
