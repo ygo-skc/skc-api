3f0f0ab8a84cb3255329f19ab32e70db6f5c4086
Javi <8519394+rtomyj@users.noreply.github.com>
2024-09-06 08:21:46 -0500

    Removed doppler usage

--- aws-cert-update.sh
+++ aws-cert-update.sh
@@ -1,4 +1,8 @@
-ARN=$(doppler secrets get -p skc-api -c prod --plain SSL_CERT_AWS_ARN)
+HOST_NAME="skc-ygo-api.com"
+
+# get ARN using the hostname, update certs using ARN
+ARN=$(aws acm list-certificates --query 'CertificateSummaryList[*].[CertificateArn,DomainName]' --output text | grep "[[:space:]]$HOST_NAME" | cut -f 1)
+echo "Updating $ARN certificate info"
 
 aws acm import-certificate --certificate-arn "$ARN" \
-  --certificate "fileb://certs/certificate.crt" --certificate-chain "fileb://certs/ca_bundle.crt" --private-key "fileb://certs/private.key"
+  --certificate "fileb://certs/certificate.crt" --certificate-chain "fileb://certs/ca_bundle.crt" --private-key "fileb://certs/private.key" > /dev/null
