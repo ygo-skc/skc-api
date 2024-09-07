833c8137dad3997288a47131245c6c72747f4ada
Javi <8519394+rtomyj@users.noreply.github.com>
2024-09-07 10:29:27 -0500

    Added pool config, added timeouts

--- src/main/kotlin/com/rtomyj/skc/config/WebClientConfig.kt
+++ src/main/kotlin/com/rtomyj/skc/config/WebClientConfig.kt
@@ -1,14 +1,22 @@
 package com.rtomyj.skc.config
 
 import com.rtomyj.skc.exception.DownStreamException
+import io.netty.channel.ChannelOption
+import io.netty.handler.timeout.ReadTimeoutHandler
+import io.netty.handler.timeout.WriteTimeoutHandler
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.web.reactive.function.client.ClientRequest
 import org.springframework.web.reactive.function.client.ClientResponse
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
+import reactor.netty.http.client.HttpClient
+import reactor.netty.resources.ConnectionProvider
+import java.time.Duration
+import java.util.concurrent.TimeUnit
 
 
 @Configuration
@@ -19,6 +27,29 @@ class WebClientConfig {
     @Value("\${api.skc-suggestion-engine.base-uri}") skcSuggestionEngineBaseUri: String
   ): WebClient = WebClient
       .builder()
+      .clientConnector(
+        ReactorClientHttpConnector(
+          HttpClient
+              .create(
+                ConnectionProvider
+                    .builder("skc-suggestion-engine-pool")
+                    .maxConnections(50)
+                    .maxIdleTime(Duration.ofMinutes(5))
+                    .maxLifeTime(Duration.ofMinutes(10))
+                    .pendingAcquireTimeout(Duration.ofSeconds(5))
+                    .evictInBackground(Duration.ofSeconds(5))
+                    .build()
+              )
+              .responseTimeout(Duration.ofMillis(250))
+              .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 250)
+              .option(ChannelOption.SO_KEEPALIVE, true)
+              .doOnConnected { conn ->
+                conn
+                    .addHandlerLast(ReadTimeoutHandler(800, TimeUnit.MILLISECONDS))
+                    .addHandlerLast(WriteTimeoutHandler(800, TimeUnit.MILLISECONDS))
+              }
+        )
+      )
       .filter(ExchangeFilterFunction.ofRequestProcessor { request ->
         Mono.fromCallable {
           ClientRequest
