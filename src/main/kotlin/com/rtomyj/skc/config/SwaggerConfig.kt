7dc0a0cbf81c4281311217789a32fb9a2aeb95ed
Javi <8519394+rtomyj@users.noreply.github.com>
2024-09-24 22:43:12 -0500

    Added generic API responses

--- src/main/kotlin/com/rtomyj/skc/config/SwaggerConfig.kt
+++ src/main/kotlin/com/rtomyj/skc/config/SwaggerConfig.kt
@@ -1,18 +1,31 @@
 package com.rtomyj.skc.config
 
+import com.rtomyj.skc.exception.SKCError
 import com.rtomyj.skc.util.constant.AppConstants
+import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.ExternalDocumentation
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.info.License
+import io.swagger.v3.oas.models.media.Content
+import io.swagger.v3.oas.models.media.MediaType
+import io.swagger.v3.oas.models.media.Schema
+import io.swagger.v3.oas.models.responses.ApiResponse
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 
 /**
  * Configuration class for Swagger UI
  */
 @Configuration
 class SwaggerConfig {
+  companion object {
+    val SKC_ERROR_PROPERTIES = mapOf("message" to Schema<String>().type("string")
+        .description("Error message"),
+      "code" to Schema<String>().type("string")
+          .description("Error code"))
+  }
 
   /**
    * Create and returns an object that has information about me.
@@ -33,10 +46,38 @@ class SwaggerConfig {
               )
               .termsOfService("https://github.com/ygo-skc/skc-api#readme")
         )
+        .components(components())
         .externalDocs(
           ExternalDocumentation()
               .description("GitHub")
               .url("https://github.com/ygo-skc/skc-api")
         )
   }
+
+  private fun components() = Components()
+      .addResponses("Bad Request", ApiResponse()
+          .description("Malformed request. Make sure request is valid JSON and data is using correct data types.")
+          .content(Content().addMediaType(APPLICATION_JSON_VALUE,
+            MediaType().schema(skcErrorSchema(SKCError(message = "Bad Request", code = "XXX"))))))
+      .addResponses("Not Found", ApiResponse()
+          .description("No resource found for requested item.")
+          .content(Content().addMediaType(APPLICATION_JSON_VALUE,
+            MediaType()
+                .schema(skcErrorSchema(SKCError(message = "Requested resource was not found", code = "DB001"))))))
+      .addResponses("Unprocessable Entity", ApiResponse()
+          .description("Request is using data that is wrong - for example using a card ID with 7 digits instead of 8.")
+          .content(Content().addMediaType(APPLICATION_JSON_VALUE,
+            MediaType()
+                .schema(skcErrorSchema(SKCError(message = "URL parameter, URL path parameter or data in body is not valid", code = "G001"))))))
+      .addResponses("Internal Server Error", ApiResponse()
+          .description("Server encountered an exception.")
+          .content(Content().addMediaType(APPLICATION_JSON_VALUE,
+            MediaType()
+                .schema(skcErrorSchema(SKCError(message = "Error occurred calling service", code = "DS001"))))))
+
+  private fun skcErrorSchema(example: SKCError) = Schema<SKCError>()
+      .type("object")
+      .description("Error response")
+      .properties(SKC_ERROR_PROPERTIES)
+      .example(example)
 }
