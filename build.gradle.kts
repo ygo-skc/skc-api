3ec010cc5d7b0dccf5688eeb58cc1b9b1f174d82
Javi <8519394+rtomyj@users.noreply.github.com>
2025-07-12 21:45:16 -0500

    Reduced instances

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ val jakartaServletApiVersion = "6.1.0"
 
 val archivesBaseName = "skc-api"
 group = "com.rtomyj.skc"
-version = "3.0.6"
+version = "3.0.7"
 java.sourceCompatibility = JavaVersion.VERSION_21
 
 plugins {
