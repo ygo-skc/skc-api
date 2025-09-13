9d20145bd26cec9f9971e0fbc703fc46d68122cc
Javi <8519394+rtomyj@users.noreply.github.com>
2025-09-12 20:14:05 -0500

    Fixing content type

--- src/main/kotlin/com/rtomyj/skc/find/BannedCardsController.kt
+++ src/main/kotlin/com/rtomyj/skc/find/BannedCardsController.kt
@@ -16,14 +16,19 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
 
 /**
  * Configures endpoint(s) that can be used to obtain information about cards for a particular ban list.
  */
 @RestController
-@RequestMapping(path = ["/ban_list"], produces = ["application/hal+json; charset=UTF-8"])
+@RequestMapping(path = ["/ban_list"], produces = ["application/json; charset=UTF-8"])
 @Validated
 @Tag(name = SwaggerConstants.BAN_LIST_TAG_NAME)
 class BannedCardsController
