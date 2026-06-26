d408e2bd5fc1308defbc980aae4718719987589d
Javi <8519394+rtomyj@users.noreply.github.com>
2026-06-25 20:18:48 -0500

    updated version

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ val commonLang3Version = "3.18.0"
 
 val archivesBaseName = "skc-api"
 group = "com.rtomyj.skc"
-version = "3.1.5"
+version = "3.1.6"
 java.sourceCompatibility = JavaVersion.VERSION_25
 
 plugins {
