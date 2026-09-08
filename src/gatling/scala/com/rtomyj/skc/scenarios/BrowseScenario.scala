459175747965926907d94bdc73fb8a83d221e40c
Javi <8519394+rtomyj@users.noreply.github.com>
2026-09-07 21:11:45 -0500

    Fixed tests

--- src/gatling/scala/com/rtomyj/skc/scenarios/BrowseScenario.scala
+++ src/gatling/scala/com/rtomyj/skc/scenarios/BrowseScenario.scala
@@ -3,13 +3,13 @@ package com.rtomyj.skc.scenarios
 import com.rtomyj.skc.config.Configuration
 import com.rtomyj.skc.requests.BrowseRequest
 import io.gatling.core.Predef._
-import io.gatling.core.feeder.BatchableFeederBuilder
+import io.gatling.core.feeder.FileBasedFeederBuilder
 import io.gatling.core.structure.ScenarioBuilder
 
 import scala.language.postfixOps
 
 object BrowseScenario {
-  val cardBrowseFeed: BatchableFeederBuilder[String] = ssv("gatling/browse_cards_three_or_more_criteria.ssv").random.circular
+  val cardBrowseFeed: FileBasedFeederBuilder[String] = ssv("gatling/browse_cards_three_or_more_criteria.ssv").random.circular
 
   val getBrowseResultsScenario: ScenarioBuilder = scenario("Card Browse Details Scenario")
     .feed(cardBrowseFeed)
